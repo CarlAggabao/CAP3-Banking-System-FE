b290fc401b95f84d5507ea88ca1d89d7cce4078e
CarlAggabao <caggabao1@vertere-gs.com>
2026-03-27 13:16:08 +0800

    feat: admin-dashboard dir structure, updated service dir structure

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import { AuthService } from '../../services/auth';
+import { AuthService } from '../../core/services/auth';
 import { CommonModule } from '@angular/common';
 
 @Component({
